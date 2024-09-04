61778fa97235b0948e6ae2e9a8cfb3739bc39557
zolfaShefreie <z.shefreie@gmail.com>
2024-09-04 17:23:48 +0330

    Update settings.py

--- settings.py
+++ settings.py
@@ -24,8 +24,8 @@ HUB_CLASSIFIER_MODEL_ID = env('HUB_CLASSIFIER_MODEL_ID', default=None)
 HUB_CLASSIFIER_PRIVATE_REPO = env('HUB_CLASSIFIER_PRIVATE_REPO', default=False)
 
 # text generator using text and audio modals
-HUB_BIMODEL_ID = env("HUB_MODEL_ID", default=None)
-HUB_BIMODEL_PRIVATE_REPO = env("HUB_MODEL_PRIVATE_REPO", default=None)
+HUB_BIMODEL_MODEL_ID = env("HUB_BIMODEL_MODEL_ID", default=None)
+HUB_BIMODEL_PRIVATE_REPO = env("HUB_BIMODEL_PRIVATE_REPO", default=None)
 
 # text generator using text of BiMEmpDialogues dataset
 HUB_TEXT_MODEL_ID = env("HUB_TEXT_MODEL_ID", default=None)
