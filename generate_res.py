3e5e75dee8ee18e56ebe1e42fc13bfdf919332ad
zolfaShefreie <z.shefreie@gmail.com>
2024-12-27 09:50:58 +0330

    complete interface

--- generate_res.py
+++ generate_res.py
@@ -1,5 +1,7 @@
-import json
-
+from model_data_process.dataset import EmpatheticDialoguesDataset
+from model_data_process.example_retriever import ExampleRetriever
+from model_data_process.knowledge_generator import KnowledgeGenerator
+from utils.audio_util import AudioModule
 from utils.interface import BaseInterface, str2bool
 from model_data_process.data_model_mapping import MultiModalResponseGeneratorMapping, TextualResponseGeneratorMapping,\
     EmotionalTextualResponseGeneratorMapping, MultiModelEmotionClassifierMapping
@@ -7,6 +9,8 @@ from utils.callbacks import SaveHistoryCallback
 
 from transformers.trainer_utils import PredictionOutput
 import os
+import json
+import pandas as pd
 
 
 class EvaluateInterface(BaseInterface):
@@ -108,16 +112,76 @@ class EvaluateInterface(BaseInterface):
                 raise Exception("invalid conversation format")
             return new_format_of_conversation(validate_conversation(content['conversation']))
 
-    def get_generated_knowledge(self):
-        pass
+    def get_generated_knowledge(self, record):
+        """
+        generate knowledge for conversation
+        :param record:
+        :return:
+        """
+        social, event, entity = KnowledgeGenerator.run(texts=record['history'])
+        record_plus_knw = {'social_rel': social,
+                           'event_rel': event,
+                           'entity_rel': entity}
+        record_plus_knw.update(record)
+        return record_plus_knw
 
-    def get_examples(self):
-        pass
+    def get_examples(self, record):
+        """
+        get nearest examples for on conversation
+        :param record: a conversation
+        :return:
+        """
+        train_df = EmpatheticDialoguesDataset.conv_preprocess(split='train', add_knowledge=True, add_examples=False)
+        train_df = pd.DataFrame(train_df)
+        train_df['xReact'] = train_df['social_rel'].apply(lambda x: list(x.values())[0]['xReact'])
+        train_df['history_str'] = train_df['history'].apply(lambda x: ", ".join(x))
+        example_retriever = ExampleRetriever(train_df=train_df, ctx_key_name='label', qs_key_name='history_str',
+                                             conv_key_name='original_conv_id')
+
+        record['history_str'] = ", ".join(record['history'])
+        record['xReact'] = list(record['social_rel'].values())[0]['xReact']
+        record = example_retriever(record)
+        return record
+
+    def process_audio(self, record):
+        record['audio'] = AudioModule.get_audio_data(file_path=record['file_path'])
+        return record
+
+    def get_generation_config(self):
+        with open(self.generation_config_path, mode='r', encoding='utf-8') as file:
+            generation_config = dict(json.loads(file.read()))
+            return generation_config
 
     def _run_main_process(self):
+
+        # conversation process
+        conversation_record = self.get_conversation()
+        conversation_record = self.get_examples(self.get_generated_knowledge(conversation_record))
+        if self.model == "BiModalResponseGenerator":
+            conversation_record = self.process_audio(conversation_record)
+
+        # load model and its mapping
         config = self.MAP_CONFIGS[self.model](include_knowledge=self.include_knowledge,
                                               include_example=self.include_example,
                                               include_emp_losses=self.include_emp_losses)
+
+        model_class = config.ModelClass
+        try:
+            model = model_class.from_pretrained(config.hub_args()['hub_model_id'], token=config.hub_args()['hub_token'])
+        except Exception:
+            raise Exception("there is no pretrained model on HuggingFace")
+        model.eval()
+
+        # preprocess conversation
+        preprocessed_conv = config.TRANSFORMS(conversation_record)
+
+        # generate response
+        generation_config = self.get_generation_config()
+        output = model.generate(**preprocessed_conv, **generation_config)
+
+        # post process and print
+        response = config.ConversationTokenizer.target_tokenizer.decode(output[0], skip_special_tokens=True)
+        print(f"Generated Response: {response}")
 
 
 if __name__ == "__main__":
